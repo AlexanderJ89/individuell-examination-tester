c5290cad3c2e1b9342d28f1dccb49063f4f2e479
Alexander <aleck_johansson@hotmail.com>
2025-12-05 09:16:03 +0100

    setup main

--- strajk-bowling/vite.config.js
+++ strajk-bowling/vite.config.js
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  test: {
+    environment: "jsdom",
+    globals: true,
+    setupFiles: ["src/mocks/setupTests.js"],
+    coverage: {
+      provider: "v8",
+      reporter: ["text", "json", "html"],
+      lines: 90,
+      functions: 90,
+      branches: 90,
+      statements: 90,
+      exclude: [
+        "node_modules/",
+        "**/__mocks__/",
+        "**/main.jsx",
+        "**vite.config.js",
+      ],
+    },
+  },
+});
